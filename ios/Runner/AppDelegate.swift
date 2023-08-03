bd3a0ca436cecf4bea1429654b2e56e4dba452ab
梅原稜 <region.pc01@gmail.com>
2023-08-03 12:23:07 +0900

    IOS用の変更

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps // 追加
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("YOUR KEY HERE") // 追加
+    GMSServices.setMetalRendererEnabled(true) // 詳細は後ほど
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
